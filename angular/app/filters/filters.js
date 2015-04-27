4b731a076dd6596ec01b3118ae3b6bbf9c235095
Laurent Bonnet <laurentf.bonnet@gmail.com>
2015-04-27 18:27:01 +0200

    test img

--- angular/app/filters/filters.js
+++ angular/app/filters/filters.js
@@ -18,11 +18,16 @@ myappFilters.filter('textFormat', function() {
 
 myApp.filter('urlToLink', function () {
     var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi;
-    var imgPattern = /\[img\](.*?)\[\/img\]/;
+    var imgPattern = /\[img\](.*?)\[\/img\]/i;
+
+	//var replace = "<a href=\""+img+"\" target=\"_blank\"><img style=\"border: 5px solid dodgerblue;\" id=\"imgB64\" src=\"$1\" alt=\"test\"></a>";
+	var imgReplace = "<img style=\"cursor: pointer; border: 5px solid dodgerblue;\" id=\"imgB64\" src=\"$1\" alt=\"test\">";
+	str = str.replace(search, replace);
 
     return function (text, target) {
     	var resp = "";
         resp += text.replace(urlPattern, '<a target="' + target + '" href="$&">$&</a>');
+        resp += text.replace(imgPattern, imgReplace);
         //resp += text.replace(imgPattern, '<a target="' + target + '" href="$&">TEST</a>');
         return resp;
     };
